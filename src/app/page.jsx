e2ca36534f6cdfb5f7590ac627c87d10c8a513c3
damderis <93331952+damderis@users.noreply.github.com>
2024-03-23 18:34:07 +0800

    Update page.jsx

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -7,7 +7,7 @@ const Homepage = () => {
   initial={{y:"-200vh"}}
   animate={{y:"0%"}}
   transition={{duration:0.7}}>
-  <div className="h-full flex flex-col lg:flex-row px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48 lg:gap-8 justify-between">
+  <div className="h-full overflow-scroll flex flex-col lg:flex-row px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48 lg:gap-8 justify-between">
       {/*image container*/}
       <div className="h-1/2 lg:h-full lg:w-1/2 relative">
         <Image src="/webport.png" alt="" fill className="object-contain"/>
