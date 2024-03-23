c0e315b7ffac335d22002497f8e0540c149a3417
damderis <93331952+damderis@users.noreply.github.com>
2024-03-23 18:32:31 +0800

    Update page.jsx

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -7,9 +7,9 @@ const Homepage = () => {
   initial={{y:"-200vh"}}
   animate={{y:"0%"}}
   transition={{duration:0.7}}>
-  <div className="h-full flex flex-col lg:flex-row px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48 lg:gap-8 p-6">
+  <div className="h-full flex flex-col lg:flex-row px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48 lg:gap-8">
       {/*image container*/}
-      <div className="h-1/2 lg:h-full lg:w-1/2 relative">
+      <div className="h-1/2 lg:h-full lg:w-1/2 relative p-16">
         <Image src="/webport.png" alt="" fill className="object-contain"/>
       </div>
       {/*text container*/}
