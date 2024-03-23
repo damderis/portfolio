efc94f5d9cfa8a96581a3ba3167172f7469335a2
damderis <93331952+damderis@users.noreply.github.com>
2024-03-23 18:29:22 +0800

    Update page.jsx

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -7,9 +7,9 @@ const Homepage = () => {
   initial={{y:"-200vh"}}
   animate={{y:"0%"}}
   transition={{duration:0.7}}>
-  <div className="h-full flex flex-col lg:flex-row px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48 lg:gap-8">
+  <div className="h-full flex flex-col lg:flex-row px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48 lg:gap-8 p-6">
       {/*image container*/}
-      <div className="h-1/2 lg:h-full lg:w-1/2 relative p-4">
+      <div className="h-1/2 lg:h-full lg:w-1/2 relative">
         <Image src="/webport.png" alt="" fill className="object-contain"/>
       </div>
       {/*text container*/}
@@ -17,7 +17,7 @@ const Homepage = () => {
         {/*title*/}
         <h1 className="w-full text-3xl md:text-4xl font-mono text-black font-black">Simple, Efficient, Responsible</h1>
         {/*description*/}
-        <p className="text-black md:text-lg text-justify font-mono">
+        <p className="text-black md:text-lg font-mono">
           Welcome to my homepage! I am Adam Idris,
           a third-year student deeply passionate 
           about blending technical skill with astute 
